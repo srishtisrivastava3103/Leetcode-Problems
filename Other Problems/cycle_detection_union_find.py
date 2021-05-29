117ecd365db603e3eb8a491cbc325b46bca94467
srishtisrivastava3103 <srishti.srivastava3103@gmail.com>
2021-05-29 12:23:44 +0530

    Added Cycle Detection using Union Find

---
+++ Other Problems/cycle_detection_union_find.py
@@ -0,0 +1,22 @@
+# Cycle Detection Algorithm using Union Find Algorithm
+
+def unionFind(graph = [[1],[0,2,3],[1],[1,4],[3]]):
+    parent = [-1]*5
+    def findparent(node):
+        if parent[node]==-1:
+            return node
+        else:
+            return findparent(parent[node])
+    def union(x,y):
+            parent[x] = y
+    def checkcycle(edges):
+        for u,v in edges:
+            x = findparent(u)
+            y =findparent(v)
+            if x==y:
+                return True
+            else:
+                union(x,y)
+        return False
+    return checkcycle([[1,2],[2,3],[2,4],[3,4],[4,5]])
+unionFind()
