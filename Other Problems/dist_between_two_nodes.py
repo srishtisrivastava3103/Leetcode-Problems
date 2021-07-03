ff31c6caac8de61780ab9ce4ab49bee4ec099864
srishtisrivastava3103 <srishti.srivastava3103@gmail.com>
2021-07-03 23:35:06 +0530

    Added Distance Between Two Nodes

---
+++ Other Problems/dist_between_two_nodes.py
@@ -0,0 +1,128 @@
+#https://practice.geeksforgeeks.org/problems/min-distance-between-two-given-nodes-of-a-binary-tree/1
+
+
+'''
+# Node Class:
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+def findDist(root,a,b):
+    
+    #return: minimum distance between a and b in a tree with given root
+    #code here
+    def pathfromroot(root,path,data):
+        if root==None:
+            return False
+        path.append(root)
+        if root.data==data:
+            return True
+        if (root.left and pathfromroot(root.left,path,data)) or (root.right and pathfromroot(root.right,path,data)):
+            return True
+        path.pop()
+        return False
+            
+    path1 = []
+    pathfromroot(root, path1, a)
+    path2 = []
+    pathfromroot(root, path2, b)
+    i = 0
+    while i<len(path1) and i<len(path2):
+        if path1[i]!=path2[i]:
+            lca = i
+            break
+        i+=1
+    return len(path1)+len(path2)-2*(i)
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+sys.setrecursionlimit(50000)
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        a, b=map(int, input().split())
+        print(findDist(root, a, b))
+
+# } Driver Code Ends
