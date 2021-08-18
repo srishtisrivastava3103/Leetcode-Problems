2879bd00ecd81410a094e6869720cbf118d95bba
srishtisrivastava3103 <srishti.srivastava3103@gmail.com>
2021-08-18 23:06:31 +0530

    Added Problem 1854 through Prefix Sum Method

--- 1854_maximum_population_year.py
+++ 1854_maximum_population_year.py
@@ -1,12 +1,32 @@
 # https://leetcode.com/problems/maximum-population-year/
 
+
+#Method - 1: Brute Force
+
+# class Solution:
+#     def maximumPopulation(self, logs: List[List[int]]) -> int:
+#         l = list(range(1950,2051))
+#         d = dict.fromkeys(l,0)
+#         # print(d)
+#         for lt in logs:
+#             for j in range(lt[0],lt[1]):
+#                 d[j]+=1
+#         return max(d,key = d.get)        
+
+# Method-2
+# Prefix Sum
+
 class Solution:
     def maximumPopulation(self, logs: List[List[int]]) -> int:
         l = list(range(1950,2051))
         d = dict.fromkeys(l,0)
         # print(d)
         for lt in logs:
-            for j in range(lt[0],lt[1]):
-                d[j]+=1
-        return max(d,key = d.get)        
-
+            d[lt[0]]+=1
+            d[lt[1]]-=1
+        maxyear = 1950
+        for i in range(1951,2051):
+            d[i] +=d[i-1] 
+            if d[i]>d[maxyear]:
+                maxyear = i
+        return maxyear
