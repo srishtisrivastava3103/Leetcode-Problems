7c03ee1eaca3986c4a7bc29314805c0246f9f59f
srishtisrivastava3103 <srishti.srivastava3103@gmail.com>
2021-08-20 10:41:03 +0530

    Better space usage for the problem

--- street_planning_problem.py
+++ street_planning_problem.py
@@ -1,19 +1,38 @@
 # https://codereview.stackexchange.com/questions/118966/solution-to-king-kohima-problem-in-python
 
-def streetPlan(n):
-    ans = []
-    def solve(n,prev):
-        if n==0:
-            ans.append(prev)
-            return 1
-        if prev[-1]=="X":
-            return solve(n-1,prev+"Y")
-        else:
-            return solve(n-1,prev+"X") and solve(n-1,prev+"Y")
+# We can solve this with better space utilization using a class variable
+class Solution():
+    def streetPlan(self,n):
+        self.count+=1
+        def solve(n,prev):
+            if n==0:
+                self.count+=1
+                return 1
+            if prev[-1]=="X":
+                return solve(n-1,prev+"Y")
+            else:
+                return solve(n-1,prev+"X") and solve(n-1,prev+"Y")
 
-    solve(n-1,"Y") 
-    print(ans)
-    solve(n-1,"X")
-    print(ans)
-    return len(ans)**2
-streetPlan(3)
+        solve(n-1,"Y") 
+        solve(n-1,"X")
+        return self.count**2
+    streetPlan(3)
+
+
+# def streetPlan(n):
+#     ans = []
+#     def solve(n,prev):
+#         if n==0:
+#             ans.append(prev)
+#             return 1
+#         if prev[-1]=="X":
+#             return solve(n-1,prev+"Y")
+#         else:
+#             return solve(n-1,prev+"X") and solve(n-1,prev+"Y")
+
+#     solve(n-1,"Y") 
+#     print(ans)
+#     solve(n-1,"X")
+#     print(ans)
+#     return len(ans)**2
+# streetPlan(3)
