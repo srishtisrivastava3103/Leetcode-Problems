0dab3fd30bd1d0c6a79de8ca8aab88baf34770d7
srishtisrivastava3103 <srishti.srivastava3103@gmail.com>
2021-08-31 16:13:58 +0530

    Added Minimum Deletions to make string palindrome

---
+++ Other Problems/minimum_deletions_to_make_a_string_palindrome.py
@@ -0,0 +1,15 @@
+#https://practice.geeksforgeeks.org/problems/minimum-deletitions1648/1
+
+def minimumNumberOfDeletions(S):
+    s2 = s[::-1]
+    dp = [[-1]*(len(s)+1) for i in range(len(s)+1)]
+    for i in range(len(s)):
+        dp[0][i] = 0
+        dp[i][0] = 0
+    for i in range(1,len(s)+1):
+        for j in range(1,len(s)+1):
+            if s[i-1]==s2[j-1]:
+                dp[i][j] = dp[i-1][j-1]+1
+            else:
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+    return len(s)-dp[-1][-1]
