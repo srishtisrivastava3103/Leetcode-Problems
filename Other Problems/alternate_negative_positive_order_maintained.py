670cb7805faf8ffb196f4e10458c4e816129ece5
srishtisrivastava3103 <srishti.srivastava3103@gmail.com>
2021-08-31 16:16:44 +0530

    Modified Alternate Positive Negative Problem

--- Other Problems/alternate_negative_positive_order_maintained.py
+++ Other Problems/alternate_negative_positive_order_maintained.py
@@ -1,14 +1,16 @@
 # https://www.geeksforgeeks.org/rearrange-array-alternating-positive-negative-items-o1-extra-space/
 
+# Time Complexity: O(n2)
+
 def rearrange(arr):
     i = 0
+    extras = []
     while i<len(arr):
         if i%2==0 and arr[i]>0:
             k = i+1
             while k<len(arr) and arr[k]>-1:
                 k+=1
             if k<len(arr):
-#                 arr[k],arr[i] = arr[i],arr[k]
                 store = arr[k]
                 while k>i:
                     arr[k] = arr[k-1]
@@ -16,7 +18,6 @@ def rearrange(arr):
                 arr[i] = store
             else:
                 break
-                
         elif i%2!=0 and arr[i]<0:
             k = i+1
             while k<len(arr) and arr[k]<0:
@@ -30,9 +31,39 @@ def rearrange(arr):
 
             else:
                 break
-                
         i+=1
-    return arr
+    arr+=extras
+    print(arr)
                         
 rearrange([-5, -2, 5, 2, 4, 7, 1, 8, 0, -8])
                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
