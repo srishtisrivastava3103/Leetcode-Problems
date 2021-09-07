9872e7e75d1e495e37948e188faea4f2c91d24c3
srishtisrivastava3103 <srishti.srivastava3103@gmail.com>
2021-09-07 15:09:18 +0530

    Added Problem 746 Top Down Approach

--- 746_min_cost_climbing_stairs.py
+++ 746_min_cost_climbing_stairs.py
@@ -28,6 +28,7 @@ class Solution:
         return costing(len(cost)-1)
 
 # Using memoization explicitly
+
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         cost+=[0]
@@ -41,3 +42,18 @@ class Solution:
             dp[i] = min(cost[i]+costing(i-1),cost[i]+costing(i-2))
             return dp[i]
         return costing(len(cost)-1)
+
+
+# Top Down Approach (DP Iterative)
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost+=[0]
+        dp = [-1]*len(cost)
+        for i in range(len(cost)):
+            if i==0 or i==1:
+                dp[i] = cost[i]
+            else:
+                dp[i] = min(cost[i]+dp[i-1],cost[i]+dp[i-2])
+        # print(dp)
+        return dp[-1]
