23e6d9c55ee992905e12862157b7be356f95c9b7
srishtisrivastava3103 <srishti.srivastava3103@gmail.com>
2021-10-02 22:27:47 +0530

    Added Problem Non Constructible Change

---
+++ Other Problems/non_constructible_change.py
@@ -0,0 +1,13 @@
+# https://www.algoexpert.io/questions/Non-Constructible%20Change
+
+def nonConstructibleChange(coins):
+    # Write your code here.
+    coins.sort()
+    change = 0
+    for coin in coins:
+        if coin>change+1:
+            return change+1
+        change += coin
+    return change+1
+            
+		
